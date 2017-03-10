dd4c988ea0130a4dca4280b54c9d52bc308b3f86
akemimadoka <jxhzq1996@126.com>
2017-03-10 18:54:34 +0900

    初始提交

---
+++ PixivClassifier/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace PixivClassifier
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必需的设计器变量。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清理所有正在使用的资源。
+		/// </summary>
+		/// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows 窗体设计器生成的代码
+
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要修改
+		/// 使用代码编辑器修改此方法的内容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.label1 = new System.Windows.Forms.Label();
+			this.txtPicturePath = new System.Windows.Forms.TextBox();
+			this.btnSelectFolder = new System.Windows.Forms.Button();
+			this.tvPictures = new System.Windows.Forms.TreeView();
+			this.progressBar1 = new System.Windows.Forms.ProgressBar();
+			this.label2 = new System.Windows.Forms.Label();
+			this.txtThreadCount = new System.Windows.Forms.TextBox();
+			this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.mnuCopySelectedItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.chkSearchRecursively = new System.Windows.Forms.CheckBox();
+			this.btnStartSearch = new System.Windows.Forms.Button();
+			this.label3 = new System.Windows.Forms.Label();
+			this.txtTimeout = new System.Windows.Forms.TextBox();
+			this.contextMenuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(8, 12);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(89, 12);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "图片文件路径：";
+			// 
+			// txtPicturePath
+			// 
+			this.txtPicturePath.Location = new System.Drawing.Point(97, 9);
+			this.txtPicturePath.Name = "txtPicturePath";
+			this.txtPicturePath.Size = new System.Drawing.Size(227, 21);
+			this.txtPicturePath.TabIndex = 1;
+			// 
+			// btnSelectFolder
+			// 
+			this.btnSelectFolder.Location = new System.Drawing.Point(330, 8);
+			this.btnSelectFolder.Name = "btnSelectFolder";
+			this.btnSelectFolder.Size = new System.Drawing.Size(40, 22);
+			this.btnSelectFolder.TabIndex = 2;
+			this.btnSelectFolder.Text = "...";
+			this.btnSelectFolder.UseVisualStyleBackColor = true;
+			this.btnSelectFolder.Click += new System.EventHandler(this.btnSelectFolder_Click);
+			// 
+			// tvPictures
+			// 
+			this.tvPictures.Location = new System.Drawing.Point(9, 89);
+			this.tvPictures.Name = "tvPictures";
+			this.tvPictures.Size = new System.Drawing.Size(360, 204);
+			this.tvPictures.TabIndex = 3;
+			this.tvPictures.MouseClick += new System.Windows.Forms.MouseEventHandler(this.tvPictures_MouseClick);
+			// 
+			// progressBar1
+			// 
+			this.progressBar1.Location = new System.Drawing.Point(10, 62);
+			this.progressBar1.Name = "progressBar1";
+			this.progressBar1.Size = new System.Drawing.Size(360, 21);
+			this.progressBar1.TabIndex = 4;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(8, 38);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(53, 12);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "线程数：";
+			// 
+			// txtThreadCount
+			// 
+			this.txtThreadCount.Location = new System.Drawing.Point(60, 35);
+			this.txtThreadCount.Name = "txtThreadCount";
+			this.txtThreadCount.Size = new System.Drawing.Size(42, 21);
+			this.txtThreadCount.TabIndex = 6;
+			this.txtThreadCount.Text = "20";
+			// 
+			// contextMenuStrip1
+			// 
+			this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuCopySelectedItem});
+			this.contextMenuStrip1.Name = "contextMenuStrip1";
+			this.contextMenuStrip1.Size = new System.Drawing.Size(181, 26);
+			this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+			// 
+			// mnuCopySelectedItem
+			// 
+			this.mnuCopySelectedItem.Name = "mnuCopySelectedItem";
+			this.mnuCopySelectedItem.Size = new System.Drawing.Size(180, 22);
+			this.mnuCopySelectedItem.Text = "复制选中内容（&C）";
+			this.mnuCopySelectedItem.Click += new System.EventHandler(this.mnuCopySelectedItem_Click);
+			// 
+			// chkSearchRecursively
+			// 
+			this.chkSearchRecursively.AutoSize = true;
+			this.chkSearchRecursively.Location = new System.Drawing.Point(194, 37);
+			this.chkSearchRecursively.Name = "chkSearchRecursively";
+			this.chkSearchRecursively.Size = new System.Drawing.Size(108, 16);
+			this.chkSearchRecursively.TabIndex = 8;
+			this.chkSearchRecursively.Text = "递归地搜索图片";
+			this.chkSearchRecursively.UseVisualStyleBackColor = true;
+			// 
+			// btnStartSearch
+			// 
+			this.btnStartSearch.Location = new System.Drawing.Point(308, 34);
+			this.btnStartSearch.Name = "btnStartSearch";
+			this.btnStartSearch.Size = new System.Drawing.Size(62, 20);
+			this.btnStartSearch.TabIndex = 9;
+			this.btnStartSearch.Text = "开始";
+			this.btnStartSearch.UseVisualStyleBackColor = true;
+			this.btnStartSearch.Click += new System.EventHandler(this.btnStartSearch_Click);
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(105, 39);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(41, 12);
+			this.label3.TabIndex = 10;
+			this.label3.Text = "超时：";
+			// 
+			// txtTimeout
+			// 
+			this.txtTimeout.Location = new System.Drawing.Point(144, 35);
+			this.txtTimeout.Name = "txtTimeout";
+			this.txtTimeout.Size = new System.Drawing.Size(42, 21);
+			this.txtTimeout.TabIndex = 11;
+			this.txtTimeout.Text = "0";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(381, 305);
+			this.Controls.Add(this.txtTimeout);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.btnStartSearch);
+			this.Controls.Add(this.chkSearchRecursively);
+			this.Controls.Add(this.txtThreadCount);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.progressBar1);
+			this.Controls.Add(this.tvPictures);
+			this.Controls.Add(this.btnSelectFolder);
+			this.Controls.Add(this.txtPicturePath);
+			this.Controls.Add(this.label1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.Name = "Form1";
+			this.Text = "Pixiv快速分类器";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.contextMenuStrip1.ResumeLayout(false);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox txtPicturePath;
+		private System.Windows.Forms.Button btnSelectFolder;
+		private System.Windows.Forms.TreeView tvPictures;
+		private System.Windows.Forms.ProgressBar progressBar1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox txtThreadCount;
+		private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem mnuCopySelectedItem;
+		private System.Windows.Forms.CheckBox chkSearchRecursively;
+		private System.Windows.Forms.Button btnStartSearch;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox txtTimeout;
+	}
+}
+
---
+++ PixivClassifier/Form1.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.Contracts;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using PixivClassifier.Properties;
+
+namespace PixivClassifier
+{
+	public partial class Form1 : Form
+	{
+		// Key: PixivID, Value: 图片文件名
+		private readonly Dictionary<string, HashSet<string>> _pixivIds = new Dictionary<string, HashSet<string>>();
+		// Key: Tag, Value: PixivID
+		private readonly Dictionary<string, HashSet<string>> _tagMap = new Dictionary<string, HashSet<string>>();
+		private readonly object _lockId = new object();
+		private readonly object _lockTagMap = new object();
+		private readonly Regex _pixivFilenamePattern = new Regex(@"(\d+)_p\d+(?:_master\d*)??\.\w+", RegexOptions.Compiled);
+		private readonly Regex _pixivTagPattern = new Regex("<li class=\"tag\">.*?<a href=.*?class=\"text\">(.*?)</a>.*?</li>", RegexOptions.Compiled);
+		private int _timeout;
+		private bool _isSearching;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private async Task<IEnumerable<string>> GetTags(string pixivId)
+		{
+			using (var httpClient = new HttpClient())
+			{
+				if (_timeout > 0)
+				{
+					httpClient.Timeout = new TimeSpan(_timeout);
+				}
+				HttpResponseMessage responseMessage;
+				try
+				{
+					responseMessage = await httpClient.GetAsync($"http://www.pixiv.net/member_illust.php?mode=medium&illust_id={pixivId}");
+					if (!responseMessage.IsSuccessStatusCode)
+					{
+						return null;
+					}
+				}
+				catch (Exception)
+				{
+					return null;
+				}
+
+				var contentString = await responseMessage.Content.ReadAsStringAsync();
+				var tags = new HashSet<string>();
+
+				foreach (var matchObj in _pixivTagPattern.Matches(contentString))
+				{
+					var match = matchObj as Match;
+					if (match == null || !match.Success)
+					{
+						continue;
+					}
+
+					var tagName = match.Groups[1].Value;
+					if (!string.IsNullOrWhiteSpace(tagName))
+					{
+						tags.Add(tagName);
+					}
+				}
+
+				return tags;
+			}
+		}
+
+		private async Task SearchTag(IEnumerable<string> allPixivIds, Action completeOneCallback)
+		{
+			if (completeOneCallback == null)
+			{
+				throw new ArgumentNullException(nameof(completeOneCallback));
+			}
+
+			Contract.EndContractBlock();
+
+			foreach (var pixivId in allPixivIds)
+			{
+				try
+				{
+					var tags = await GetTags(pixivId);
+					if (tags == null)
+					{
+						continue;
+					}
+
+					foreach (var tag in tags)
+					{
+						lock (_lockTagMap)
+						{
+							if (!_tagMap.TryGetValue(tag, out var pixivIds))
+							{
+								pixivIds = new HashSet<string>();
+								_tagMap.Add(tag, pixivIds);
+							}
+
+							pixivIds.Add(pixivId);
+						}
+					}
+				}
+				finally
+				{
+					completeOneCallback();
+				}
+			}
+		}
+
+		private void btnSelectFolder_Click(object sender, EventArgs e)
+		{
+			using (var folderBrowserDialog = new FolderBrowserDialog())
+			{
+				folderBrowserDialog.SelectedPath = @"D:\temp\pic";
+				var result = folderBrowserDialog.ShowDialog();
+				if (result == DialogResult.OK && Directory.Exists(folderBrowserDialog.SelectedPath))
+				{
+					txtPicturePath.Text = folderBrowserDialog.SelectedPath;
+				}
+			}
+		}
+
+		private void tvPictures_MouseClick(object sender, MouseEventArgs e)
+		{
+			if (e.Button != MouseButtons.Right)
+			{
+				return;
+			}
+			
+			contextMenuStrip1.Show(MousePosition);
+		}
+
+		private void mnuCopySelectedItem_Click(object sender, EventArgs e)
+		{
+			if (tvPictures.SelectedNode == null)
+			{
+				return;
+			}
+
+			try
+			{
+				Clipboard.SetText(tvPictures.SelectedNode.Text);
+			}
+			catch (Exception exception)
+			{
+				MessageBox.Show(string.Format(Resources.CopyToClipboardFailedDueTo, exception), Resources.MessageError, MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+		}
+
+		private async void btnStartSearch_Click(object sender, EventArgs e)
+		{
+			if (_isSearching)
+			{
+				return;
+			}
+			
+			if (!int.TryParse(txtThreadCount.Text, out var threadCount))
+			{
+				MessageBox.Show(Resources.ThreadCountIsNotValidNumber, Resources.MessageError, MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+
+			if (threadCount <= 0 || threadCount > 100)
+			{
+				MessageBox.Show(Resources.ThreadCountOutOfAcceptableRange, Resources.MessageError, MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+
+			if (!int.TryParse(txtTimeout.Text, out _timeout))
+			{
+				MessageBox.Show(Resources.TimeoutIsNotValidNumber, Resources.MessageError, MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+
+			if (_timeout < 0)
+			{
+				MessageBox.Show(Resources.TimeoutOutOfAcceptableRange, Resources.MessageError, MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+
+			if (!Directory.Exists(txtPicturePath.Text))
+			{
+				MessageBox.Show(Resources.PicturePathNotValidOrNotExist, Resources.MessageError, MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+
+			_isSearching = true;
+
+			lock (_lockId)
+			lock (_lockTagMap)
+			{
+					_pixivIds.Clear();
+					_tagMap.Clear();
+			}
+			
+			var filenames = new HashSet<string>();
+			foreach (var filename in
+						Directory.EnumerateFiles(txtPicturePath.Text, "*",
+						chkSearchRecursively.Checked ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly))
+			{
+				filenames.Add(filename);
+			}
+
+			lock (_lockId)
+			{
+				foreach (var filename in filenames)
+				{
+					var match = _pixivFilenamePattern.Match(filename);
+					if (!match.Success)
+					{
+						continue;
+					}
+
+					var pixivId = match.Groups[1].Value;
+					if (!_pixivIds.TryGetValue(pixivId, out var pixivPictures))
+					{
+						pixivPictures = new HashSet<string>();
+						_pixivIds.Add(pixivId, pixivPictures);
+					}
+					pixivPictures.Add(filename);
+				}
+			}
+
+			var allPixivIds = _pixivIds.Keys;
+
+			tvPictures.Nodes.Clear();
+			txtPicturePath.Enabled = txtThreadCount.Enabled = txtTimeout.Enabled = btnSelectFolder.Enabled = btnStartSearch.Enabled = chkSearchRecursively.Enabled = false;
+
+			progressBar1.Value = 0;
+			progressBar1.Minimum = 0;
+			progressBar1.Maximum = allPixivIds.Count;
+
+			threadCount = Math.Min(allPixivIds.Count, threadCount);
+
+			var filesPerThread = allPixivIds.Count / threadCount;
+			var remainedFiles = allPixivIds.Count % threadCount;
+
+			var allTask = new List<Task>(threadCount);
+			for (var i = 0; i < threadCount; ++i)
+			{
+				var currentThread = i;
+				var cachedThreadCount = threadCount;
+				allTask.Add(SearchTag(
+					allPixivIds.Skip(currentThread * filesPerThread)
+						.Take((currentThread == cachedThreadCount - 1 ? remainedFiles : 0) + filesPerThread),
+					() => { progressBar1.Invoke((Action)(() => { ++progressBar1.Value; })); }));
+			}
+
+			await Task.Run(() =>
+			{
+				foreach (var task in allTask)
+				{
+					task.Wait();
+				}
+
+				Invoke((Action)(() =>
+				{
+					lock (_lockTagMap)
+					{
+						foreach (var tagPair in _tagMap)
+						{
+							var node = tvPictures.Nodes.Add(tagPair.Key);
+							foreach (var pixivIds in tagPair.Value)
+							{
+								node.Nodes.Add(pixivIds);
+							}
+						}
+
+						txtPicturePath.Enabled = txtThreadCount.Enabled = txtTimeout.Enabled = btnSelectFolder.Enabled = btnStartSearch.Enabled = chkSearchRecursively.Enabled = true;
+					}
+				}));
+				_isSearching = false;
+			});
+		}
+
+		private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+		{
+			mnuCopySelectedItem.Enabled = tvPictures.SelectedNode != null;
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			var toolTip = new ToolTip
+			{
+				ReshowDelay = 100,
+				InitialDelay = 100
+			};
+			toolTip.SetToolTip(txtThreadCount, Resources.ThreadCountToolTip);
+			toolTip.SetToolTip(txtTimeout, Resources.TimeoutToolTip);
+		}
+	}
+}
